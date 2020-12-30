3886f1ebd662d198f87ca482a47dc7a665fff508
theVytch <edu.vytch@gmail.com>
2020-12-30 20:02:58 -0300

    primeira classe posicao

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,8 +1,12 @@
 package aplicacao;
 
+import mesaJogo.Posicao;
+
 public class Programa {
 
     public static void main(String[] args) {
 
+        Posicao posicao = new Posicao(3, 8);
+        System.out.println(posicao);
     }
 }
