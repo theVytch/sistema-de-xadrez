e283f584869bedc677f0d197746daac6b3fff33a
theVytch <edu.vytch@gmail.com>
2020-12-30 20:15:06 -0300

    implementado mesa e peca

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,12 +1,12 @@
 package aplicacao;
 
 import mesaJogo.Posicao;
+import mesaJogo.Tabuleiro;
 
 public class Programa {
 
     public static void main(String[] args) {
 
-        Posicao posicao = new Posicao(3, 8);
-        System.out.println(posicao);
+        Tabuleiro tab = new Tabuleiro(8, 8);
     }
 }
